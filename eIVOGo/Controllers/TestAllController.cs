2d52940c1a33f1506e95ab99106bc20978b72ef7
rsung1973 <rsung1973@hotmail.com>
2016-11-18 11:39:40 +0800

    [2016-11-18]:update

---
+++ eIVOGo/Controllers/TestAllController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Linq;
+using System.Data.SqlClient;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+using Business.Helper;
+using ClosedXML.Excel;
+using eIVOGo.Helper;
+using eIVOGo.Models;
+using Model.DataEntity;
+using Model.Locale;
+using Model.Security.MembershipManagement;
+using Utility;
+
+namespace eIVOGo.Controllers
+{
+    public class TestAllController : SampleController<InvoiceItem>
+    {
+        // GET: TestAll
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
